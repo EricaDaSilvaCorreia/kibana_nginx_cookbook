e721be3415271a5e14ef3ac8797e173f44bc92a6
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2018-11-21 10:26:36 +0000

    MAAK 0.1 : Added what was previously done up to the point where cookbook stopped working to see if anything was mistakenly removed in the process

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,65 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+
+apt_update 'update_sources' do
+  action :update
+end
+
+#=======NGINX=========
+
+package "nginx"
+
+service "nginx" do
+  supports status: true, restart: true, reload: true
+  action [:enable, :start]
+end
+
+template '/etc/nginx/sites-available/proxy.conf' do
+  source 'proxy.conf.erb'
+  variables proxy_port: node['nginx']['proxy_port']
+  notifies :restart, 'service[nginx]'
+end
+
+link '/etc/nginx/sites-enabled/proxy.conf' do
+  to '/etc/nginx/sites-available/proxy.conf'
+  notifies :restart, 'service[nginx]', :delayed
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+  notifies :restart, 'service[nginx]', :delayed
+end
+
+#=======KIBANA=========
+
+execute "elasticsearch key" do
+  command 'wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -'
+end
+
+execute "install kibana" do
+  command 'echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list'
+end
+
+execute "update all" do
+  command "sudo apt-get update"
+end
+
+execute "install kibana" do
+  command 'apt-get install kibana -y'
+end
+
+service 'kibana' do
+  supports status: true, restart: true, reload: true
+  action [:enable, :start]
+end
+
+template '/etc/kibana/kibana.yml' do
+  source 'kibana.yml.erb'
+  notifies :restart, 'service[kibana]'
+end
+
+execute "start kibana" do
+  command "sudo service kibana start"
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,7 +11,7 @@ describe 'kibana_nginx::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
@@ -20,16 +20,19 @@ describe 'kibana_nginx::default' do
     end
   end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
-    end
+  it "runs apt get update" do
+    expect(chef_run).to update_apt_update 'update_sources'
+  end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+ it "should create file kibana.yml in /etc/kibana/" do
+    expect(chef_run).to create_template('/etc/kibana/kibana.yml')
+  end
+
+  it 'should enable the kibana service' do
+    expect(chef_run).to enable_service "kibana"
+  end
+
+  it 'should start the kibana service' do
+    expect(chef_run).to start_service "kibana"
   end
 end
